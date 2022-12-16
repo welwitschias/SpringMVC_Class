f11933d93d604cfb5620745a7550ab06da379f0b
apple <welwitschia_@naver.com>
2022-12-16 16:07:53 +0900

    plus testdata.sql

---
+++ MiniProject/testdata.sql
@@ -0,0 +1,26 @@
+DECLARE
+    cnt NUMBER := 0;
+BEGIN
+    LOOP
+        EXIT WHEN cnt > 999;
+        cnt := cnt + 1;
+        INSERT INTO content_table (
+            content_idx,
+            content_subject,
+            content_text,
+            content_file,
+            content_writer_idx,
+            content_board_idx,
+            content_date
+        ) VALUES (
+            content_seq.NEXTVAL,
+            '테스트 제목 ' || to_char(cnt),
+            '테스트 내용 ' || to_char(cnt),
+            NULL,
+            1,
+            1,
+            sysdate
+        );
+
+    END LOOP;
+END;
